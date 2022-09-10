1b261b81c45055b1ffe1f5a8a9a1a04de07d197b
simon busch <simon.busch@live.fr>
2022-09-10 08:30:50 +0200

    init

---
+++ readme.md
@@ -0,0 +1,6 @@
+Installing dependencies with forge
+
+
+-> this doesn't work with npm but directly with github
+
+ex: forge install openzeppelin/openzeppelin-contracts
---
+++ src/StakeContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contract/token/ERC20/ERC20.sol";
+
+contract StakeContract is ERC20 {
+  
+}
